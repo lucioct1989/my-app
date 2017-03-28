1c2fd409f4839806f8158e42886cbea8c26c6bc2
Luciano Patanè <lucioct89@gmail.com>
2017-03-29 00:49:22 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AdminModule } from './admin/admin.module';
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { AdminComponent } from './admin/admin.component';
+import { ConvertToEuroPipe } from './convert-to-euro.pipe';
+
+import { BackendApiService } from './backend-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    AdminComponent,
+    ConvertToEuroPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AdminModule
   ],
-  providers: [],
+  providers: [BackendApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
